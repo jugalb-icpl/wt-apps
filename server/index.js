7c8f52058f3f12fcab052f6618671ae9ce85c839
Jugal Bhagat <jugalb@infopercept.com>
2024-12-21 15:48:12 +0530

    made a little change in server/index.js

--- server/index.js
+++ server/index.js
@@ -118,5 +118,6 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/api`);
   console.log(`watchtower api: ${process.env.WATCHTOWER_API_URL}`);
+  console.log(`Job Done: True`);
   console.log(`watchtower token: ${process.env.WATCHTOWER_API_TOKEN}`);
 });
