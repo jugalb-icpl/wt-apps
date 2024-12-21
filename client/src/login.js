de799ecd9f7cef51efc69c54356395a1fbab98c9
Jugal Bhagat <jugalb@infopercept.com>
2024-12-22 01:05:05 +0530

    chnage in login and dockerfile to handle argument

--- client/src/login.js
+++ client/src/login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
 console.log("Environment Variable - API URL:", process.env.REACT_APP_API_URL);
 
 const LoginPage = () => {
